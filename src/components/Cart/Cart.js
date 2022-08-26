55df40cbeaf403639120b96819f9aed99f988b08
Abdul Ahad <ahmmedsumon087@gmai.com>
2022-08-26 09:32:32 +0600

    added items to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({cart, handleToRemove}) => {
        return (
               <div>
                      <h1>This is Cart Components</h1>
+                     {
+                            cart.map(tShirt=> <p>{tShirt.name}
+                            <button onClick={() => handleToRemove(tShirt)}>x</button>
+                            </p>)
+                     }
               </div>
        );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTShirts from '../../hooks/useTshirts';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
 import './Home.css'
 
 const Home = () => {
-       const [TShirts, setTShirts] =useTShirts();
+       const [TShirts, setTShirts] = useTShirts();
+       const [cart, setCart] = useState([]);
+
+       const handleAddToCart = (selectedItem) => {
+              const newCart = [...cart, selectedItem];
+              setCart(newCart);
+       }
+
+       const handleToRemove = (selectedItem) => {
+              
+       }
        return (
               <div className='home-container'>
                      <div className='tshirt-container'>
-                           {
-                            TShirts.map(tShirt => <TShirt
-                            key={tShirt._id}
-                            tShirt={tShirt}
-                            ></TShirt>)
-                           }
+                            {
+                                   TShirts.map(tShirt => <TShirt
+                                          key={tShirt._id}
+                                          tShirt={tShirt}
+                                          handleAddToCart={handleAddToCart}
+                                   ></TShirt>)
+                            }
                      </div>
                      <div className='cart-container'>
-                            <Cart></Cart>
+                            <Cart
+                                   handleToRemove={handleToRemove}
+                                   cart={cart}>
+
+                            </Cart>
                      </div>
               </div>
        );
 };
 
-export default Home;
+export default Home; 
